9a11acb0b816090314d8d4ce462682b7fc4dc562
emeath <matheus.martins.jeronimo@gmail.com>
2022-10-09 01:47:49 -0300

    adds output print

--- ZooProject/ZooProject.mk
+++ ZooProject/ZooProject.mk
@@ -60,7 +60,7 @@ AS       := /usr/bin/as
 ## User defined environment variables
 ##
 CodeLiteDir:=/usr/share/codelite
-Objects0=$(IntermediateDirectory)/Animal.cpp$(ObjectSuffix) $(IntermediateDirectory)/Bird.cpp$(ObjectSuffix) $(IntermediateDirectory)/Cat.cpp$(ObjectSuffix) $(IntermediateDirectory)/Dog.cpp$(ObjectSuffix) $(IntermediateDirectory)/main.cpp$(ObjectSuffix) 
+Objects0=$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IntermediateDirectory)/Dog.cpp$(ObjectSuffix) $(IntermediateDirectory)/Cat.cpp$(ObjectSuffix) $(IntermediateDirectory)/Bird.cpp$(ObjectSuffix) $(IntermediateDirectory)/Animal.cpp$(ObjectSuffix) 
 
 
 
@@ -91,29 +91,13 @@ PreBuild:
 ##
 ## Objects
 ##
-$(IntermediateDirectory)/Animal.cpp$(ObjectSuffix): Animal.cpp $(IntermediateDirectory)/Animal.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/mm/Workspaces/ws-cpp/zoo-project-cpp/ZooProject/Animal.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Animal.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/Animal.cpp$(DependSuffix): Animal.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Animal.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Animal.cpp$(DependSuffix) -MM Animal.cpp
+$(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp $(IntermediateDirectory)/main.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/mm/Workspaces/ws-cpp/zoo-project-cpp/ZooProject/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main.cpp$(DependSuffix): main.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM main.cpp
 
-$(IntermediateDirectory)/Animal.cpp$(PreprocessSuffix): Animal.cpp
-	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Animal.cpp$(PreprocessSuffix) Animal.cpp
-
-$(IntermediateDirectory)/Bird.cpp$(ObjectSuffix): Bird.cpp $(IntermediateDirectory)/Bird.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/mm/Workspaces/ws-cpp/zoo-project-cpp/ZooProject/Bird.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Bird.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/Bird.cpp$(DependSuffix): Bird.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Bird.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Bird.cpp$(DependSuffix) -MM Bird.cpp
-
-$(IntermediateDirectory)/Bird.cpp$(PreprocessSuffix): Bird.cpp
-	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Bird.cpp$(PreprocessSuffix) Bird.cpp
-
-$(IntermediateDirectory)/Cat.cpp$(ObjectSuffix): Cat.cpp $(IntermediateDirectory)/Cat.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/mm/Workspaces/ws-cpp/zoo-project-cpp/ZooProject/Cat.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Cat.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/Cat.cpp$(DependSuffix): Cat.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Cat.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Cat.cpp$(DependSuffix) -MM Cat.cpp
-
-$(IntermediateDirectory)/Cat.cpp$(PreprocessSuffix): Cat.cpp
-	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Cat.cpp$(PreprocessSuffix) Cat.cpp
+$(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) main.cpp
 
 $(IntermediateDirectory)/Dog.cpp$(ObjectSuffix): Dog.cpp $(IntermediateDirectory)/Dog.cpp$(DependSuffix)
 	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/mm/Workspaces/ws-cpp/zoo-project-cpp/ZooProject/Dog.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Dog.cpp$(ObjectSuffix) $(IncludePath)
@@ -123,13 +107,29 @@ $(IntermediateDirectory)/Dog.cpp$(DependSuffix): Dog.cpp
 $(IntermediateDirectory)/Dog.cpp$(PreprocessSuffix): Dog.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Dog.cpp$(PreprocessSuffix) Dog.cpp
 
-$(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp $(IntermediateDirectory)/main.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/mm/Workspaces/ws-cpp/zoo-project-cpp/ZooProject/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/main.cpp$(DependSuffix): main.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM main.cpp
+$(IntermediateDirectory)/Cat.cpp$(ObjectSuffix): Cat.cpp $(IntermediateDirectory)/Cat.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/mm/Workspaces/ws-cpp/zoo-project-cpp/ZooProject/Cat.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Cat.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/Cat.cpp$(DependSuffix): Cat.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Cat.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Cat.cpp$(DependSuffix) -MM Cat.cpp
 
-$(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
-	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) main.cpp
+$(IntermediateDirectory)/Cat.cpp$(PreprocessSuffix): Cat.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Cat.cpp$(PreprocessSuffix) Cat.cpp
+
+$(IntermediateDirectory)/Bird.cpp$(ObjectSuffix): Bird.cpp $(IntermediateDirectory)/Bird.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/mm/Workspaces/ws-cpp/zoo-project-cpp/ZooProject/Bird.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Bird.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/Bird.cpp$(DependSuffix): Bird.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Bird.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Bird.cpp$(DependSuffix) -MM Bird.cpp
+
+$(IntermediateDirectory)/Bird.cpp$(PreprocessSuffix): Bird.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Bird.cpp$(PreprocessSuffix) Bird.cpp
+
+$(IntermediateDirectory)/Animal.cpp$(ObjectSuffix): Animal.cpp $(IntermediateDirectory)/Animal.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/mm/Workspaces/ws-cpp/zoo-project-cpp/ZooProject/Animal.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Animal.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/Animal.cpp$(DependSuffix): Animal.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Animal.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Animal.cpp$(DependSuffix) -MM Animal.cpp
+
+$(IntermediateDirectory)/Animal.cpp$(PreprocessSuffix): Animal.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Animal.cpp$(PreprocessSuffix) Animal.cpp
 
 
 -include $(IntermediateDirectory)/*$(DependSuffix)
